8424930dd861cec90891e0f3b4d98d2abd7f5dc0
malted <malted@malted.dev>
2022-02-14 21:53:16 +0000

    :rocket: Add PM2 process config

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	apps: [
+		{
+			name: "wordle-list",
+			script: "./server.ts",
+			interpreter: "deno",
+			interpreterArgs: "run --allow-net --allow-read",
+		},
+	],
+};
+  
