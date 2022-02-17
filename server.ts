0c74024889d8edc0ec4b112f53526cebc61a78e4
Malted <malted@malted.dev>
2022-02-17 00:46:24 +0000

    :ambulance: Enable CORS for all endpoints

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import router from "./routes.ts";
 
 const PORT = 3001;
 const app = new Application();
 
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
